eb68d07eedd36b4082066a4abf45202b184a1ad6
Roman Matsypula <ukrmatsypula@gmail.com>
2023-05-27 15:07:38 +0300

    add header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="bg-gray-900 flex">Home</div>
+  <div class="container mx-auto flex justify-between items-center border-b border-gray-500 py-2">
+    <Navbar />
+    <Searchbar />
+  </div>
 </template>
 
 <script>
+import Navbar from "@/components/header/Navbar";
+import Searchbar from "@/components/header/Searchbar";
+
 export default {
   name: "h-home",
+  components: { Navbar, Searchbar },
 };
 </script>
 
---
+++ src/components/header/Searchbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex">
+    <input
+      type="text"
+      class="rounded-full bg-gray-600 px-5 mr-5 focus:outline-none focus:outline-shadow"
+      placeholder="Search..."
+    />
+
+    <img
+      id="avatar"
+      src="https://this-person-does-not-exist.com/img/avatar-gen4d1ccaca85816321cdc875ce6597954f.jpg"
+      class="max-w-50 h-10 rounded-full"
+      alt="user"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "h-SearchBar",
+};
+</script>
+
+<style scoped></style>
