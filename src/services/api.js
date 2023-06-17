93d4cf2e80092794e7d2fda2419eee3bf9c62272
Roman Matsypula <ukrmatsypula@gmail.com>
2023-06-17 15:03:49 +0300

    add .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjMyMWViYzA4MzBhNTlkZTI5MjE3OWIxYTRkMzI1NyIsInN1YiI6IjY0NzBlOTMyNzcwNzAwMDBmYzAyNzNkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tvDNjbWbVn8UHcSYlcT5EdzQmJIVO13JvYisZAVAORY";
-
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${process.env.VUE_APP_TMDP_TOKEN}`,
   },
 });
